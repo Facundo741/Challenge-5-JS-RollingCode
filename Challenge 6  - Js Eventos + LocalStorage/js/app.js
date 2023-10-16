a9bbb33722528c96deb4be12940096e5e111acd6
Facundo <santillanfacundo43@gmail.com>
2023-10-16 20:57:43 -0300

    update

--- Challenge 6  - Js Eventos + LocalStorage/js/app.js
+++ Challenge 6  - Js Eventos + LocalStorage/js/app.js
@@ -1,64 +1,49 @@
-  // Inicializar el array de usuarios desde localStorage
-  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-  document.getElementById("guardarDatos").addEventListener("click", function () {
-      // Obtener los valores del formulario
-      const nombre = document.getElementById("nombre").value;
-      const apellido = document.getElementById("apellido").value;
-      const fechaNacimiento = document.getElementById("f_Nacimiento").value;
-      const email = document.getElementById("email").value;
-      const genero = document.getElementById("genero").value;
-      const foto = document.getElementById("foto").value;
+document.getElementById("guardarDatos").addEventListener("click",(e) =>{
 
-      // Validar que los campos no estén vacíos
-      if (!nombre || !apellido || !fechaNacimiento || !email || !genero || !foto) {
-          alert("Por favor, complete todos los campos.");
-          return;
-      }
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const f_Nacimiento = document.getElementById("f_Nacimiento").value;
+    const email = document.getElementById("email").value;
+    const genero = document.getElementById("genero").value;
+    const foto = document.getElementById("foto").value;
 
-      // Crear un objeto con los datos del usuario
-      const usuario = {
-          nombre,
-          apellido,
-          fechaNacimiento,
-          email,
-          genero,
-          foto,
-      };
+    const usuario = {
+        nombre,
+        apellido,
+        f_Nacimiento,
+        email,
+        genero,
+        foto,
+    };
 
-      // Agregar el usuario al array de usuarios
-      usuarios.push(usuario);
+    usuarios.push(usuario);
 
-      // Guardar el array de usuarios en localStorage
-      localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-      // Mostrar las tarjetas de todos los usuarios
-      mostrarUsuarios();
-  });
+});
 
-  function mostrarUsuarios() {
-      const userCards = document.getElementById("userCards");
-      userCards.innerHTML = ''; // Limpiar las tarjetas existentes
+function mostrar() {
+    const cards = document.getElementById("cards");
+    cards.innerHTML = '';
 
-      // Recorrer el array de usuarios y mostrar una tarjeta por cada uno
-      usuarios.map((usuario, i) => {
-          const card = document.createElement("div");
-          card.className = "user-card";
+    usuarios.map((usuario, i) => {
+        const card = document.createElement("div");
 
-          // Crear una tarjeta con la información del usuario
-          card.innerHTML = `
-            
-              <h3>${usuario.nombre} ${usuario.apellido}</h3>
-              <p><strong>Fecha de Nacimiento:</strong> ${usuario.f_Nacimiento}</p>
-              <p><strong>Email:</strong> ${usuario.email}</p>
-              <p><strong>Género:</strong> ${usuario.genero}</p>
-              <img class= "img-fluid"src="${usuario.foto}" alt="Foto de perfil">
-            
-          `;
+        card.className = "user-card";
 
-          userCards.appendChild(card);
-      });
-  }
-
-  // Mostrar las tarjetas de los usuarios almacenados en localStorage al cargar la página
-  mostrarUsuarios();
+        card.innerHTML = `
+        <div class="card mt-5 w-50">
+        <div class="card-body">
+        <h5 class="card-title">${usuario.nombre} ${usuario.apellido}</h5>
+        <p class="card-text">Fecha de Nacimiento: ${usuario.f_Nacimiento}</p>
+        <p class="card-text">Email: ${usuario.email}</p>
+        <p class="card-text">Género: ${usuario.genero}</p>
+        </div>
+        <img src="${usuario.foto}" class="card-img-top" alt="Foto de perfil">
+        </div>`;
+        cards.appendChild(card);
+    });
+}
+mostrar();
